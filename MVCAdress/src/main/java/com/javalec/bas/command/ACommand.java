4ee8267a418cd50279cb233ed7ecad62fe077143
monargent0 <owjddms1118@gmail.com>
2022-03-28 14:01:27 +0900

    test delete
    test delete

--- MVCAdress/src/main/java/com/javalec/bas/command/ACommand.java
+++ MVCAdress/src/main/java/com/javalec/bas/command/ACommand.java
@@ -5,7 +5,4 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface ACommand {
 	public void execute(HttpServletRequest request, HttpServletResponse response);
-	// githubcommittest
-	// gitpushtest
-
 }
